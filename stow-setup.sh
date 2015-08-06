4c9398581b3a99404399230e8865fb0007c50235
Matt Thompson <matt@britecore.com>
2015-08-05 19:10:28 -0500

    Fixing the config set

--- stow-setup.sh
+++ stow-setup.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 if [ -f ~/.gitconfig ]; then
-    name="`git-config user.name`"
-    email="`git-config user.email`"
+    name="`git config --global user.name`"
+    email="`git config --global user.email`"
     rm ~/.gitconfig
 fi
 
 stow -t ~ --override=".*" tmux git vim zsh dircolors emacs vimrc_custom
+
+if [ -z "$name" ]; then
+    git config --global --replace-all user.name "$name"
+    git config --global --replace-all user.email "$email"
+fi
